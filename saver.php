ba33800752cf82d26bfbc6ca6a5e34d8b7dab14e
pthoelken <patrick.thoelken@outlook.com>
2017-03-18 17:27:23 +0100

    Fix errors in server variables

--- saver.php
+++ saver.php
@@ -1,25 +1,27 @@
 <?php
-$objConnection = new mysqli('localhost', 'root', '', 'dbiplog');
+$objConnection = new mysqli('<DATABASE_HOST>', '<DATABASE_USER>', '<DATABASE_PASSWORD>', '<DATABASE_NAME>');
 
 $strTable       = 'tblLogging';
 
+// Check your server if these $SERVER-variables are available
 $strIP          = $_SERVER['REMOTE_ADDR'];
-$strCountry     = implode ('', file ("http://ip-api.com/php/". $strIP . "?fields=countryCode"));
-// NOT WORK ATM $strOrigin      = $_SERVER['HTTP_REFERER'];
-$strOrigin      = "UC";
+$strOrigin      = $_SERVER['HTTP_REFERER'];
+$strCountry     = implode ('', file ("http://ip-api.com/csv/". $strIP . "?fields=countryCode"));
 $strHash        = hash('sha512', $strIP);
 
 $objQuery = "INSERT INTO $strTable (ip, country, origin, hash) VALUES ('$strIP', '$strCountry', '$strOrigin', '$strHash')";
 
 if (!$objConnection) {
-    die('Could not connect: ' . mysqli_error());
+    die('Could not connect database: ' . mysqli_error());
 }
 
-
 if (mysqli_query($objConnection, $objQuery)) {
-    echo "New record created successfully";
+    // You don't should display any messages here. If you show the message on the user screen the user knows that you
+    // save there privacy information.
 } else {
-    echo "Error: " . $objQuery . "<br>" . mysqli_error($objConnection);
+    // Disable the error messages in these section. It's only for debugging.
+    // If you don't disable, your users knows that you don't have a connection to your database server.
+    // echo "Error: " . $objQuery . "<br>" . mysqli_error($objConnection);
 }
 
 mysqli_close($objConnection);
